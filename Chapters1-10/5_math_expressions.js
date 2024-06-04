92f2af632acd665657ab73439725477a640e6ea1
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2024-06-04 08:25:50 -0700

    5 Chapters Completed

--- Chapters1-10/5_math_expressions.js
+++ Chapters1-10/5_math_expressions.js
@@ -1,1 +1,79 @@
 // Chp 05 : Math Expresions : familier operators
+
+
+// var popularNumer = 4;
+
+// var popularNumer = 2 + 2;
+
+// alert(2 + 2);
+
+// var popularNumer = 12 - 24;     // -12
+
+// console.log(popularNumer);
+
+
+// var popularNumer = 3 * 12;      // 36
+
+// var num = 10;
+
+// var popularNumer = num + 200;
+
+
+// var num = 10;
+
+// var anotherNum = 1;
+
+// var popularNumer = num + anotherNum;      // 11
+
+
+// var whatsLeftOver = 10 % 3;        // answer is remainder   1
+
+// var whatsLeftOver = 9 % 3;        // answer is remainder   0
+
+
+
+// MCQ's
+
+
+// 01 : % is the modulus operator.
+
+// a. true *          b. false
+
+
+
+// 02 : It gives you the remainder when the division is executed.
+
+// a. true *           b. false
+
+
+
+// 03 : It doesn't give you the result of dividing one number by another
+
+// a. true *             b. false
+
+
+
+// 04 : What is the following code is the Math epressions
+
+// var num = 12;
+
+// var num2 = 23;
+
+// var result = num + num2;
+
+// a. true *         b. false
+
+
+
+// 05 : What is the output of the following code
+
+var num = 12;
+
+var anotherNum = 2;
+
+var result = num % anotherNum;
+
+console.log(result);
+
+
+// a. 0 *        b. 1          c. 6             d. 24
