afb6967d99dd135f120342bf735821f375bfdb30
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2024-06-06 06:34:32 -0700

    Updated

--- Chapters11-20/7_removing_extracting.js
+++ Chapters11-20/7_removing_extracting.js
@@ -17,3 +17,63 @@
 
 
 // var noPets = pets.slice(2, 4);
+
+
+
+// MCQ's
+
+
+// 05 : What is the output of the following code
+
+// var arr = [1, 2, 3, 4];
+
+// var newArr = arr.slice(1, 3);
+
+// alert(newArr);
+
+// a. [1, 2, 3, 4]       b. [2, 3] *         c. []      d. [1, 3, 4]
+
+
+// 04 : What is the output of the following code
+
+// var arr = [1, 2, 3, 4];
+
+// var newArr = arr.slice(1);
+
+// alert(newArr);
+
+// a. [1, 2, 3, 4]       b. [2, 3, 4] *         c. []      d. [1, 3, 4]
+
+
+
+// 03 : What is the output of the following code
+
+// var arr = [1, 2, 3, 4];
+
+// arr.splice(1, 2);
+
+// alert(arr);
+
+// a. [1, 2, 3, 4]       b. [1, 3] *         c. [1, 4] *     d. [1, 3, 4]
+
+
+// 02 : What is the output of the following code
+
+// var arr = [1, 2, 3, 4];
+
+// arr.unshift(5);
+
+// alert(arr);
+
+// a. [5, 1, 2, 3, 4] *      b. [1, 2, 3, 4]       c. []      d. [1, 3, 4]
+
+
+// 01 : What is the output of the following code
+
+// var arr = [1, 2, 3, 4];
+
+// arr.shift();
+
+// alert(arr);
+
+// a. [1, 2, 3, 4]       b. [2, 3, 4] *         c. []      d. [1, 3, 4]
---
+++ Chapters11-20/8_for_loops.js
@@ -0,0 +1,34 @@
+// CHp 18 : for loops
+
+
+
+// var cleanestCities = ["Cheyenne", "Santa Fe", "Tucson", "Great Falls", "Honolulu"];
+
+
+// if (cityToCheck === cleanestCities[0]) {
+//     alert("It's one of the cleanest cities");
+// }
+// else if (cityToCheck === cleanestCities[1]) {
+//     alert("It's one of the cleanest cities"); }
+// else if (cityToCheck === cleanestCities[2]) {
+//     alert("It's one of the cleanest cities");
+// }
+// else if (cityToCheck === cleanestCities[3]) {
+//     alert("It's one of the cleanest cities");
+// }
+// else if (cityToCheck === cleanestCities[4]) {
+//     alert("It's one of the cleanest cities");
+// }
+// else {
+//     alert("It's not on the list");
+// }
+
+
+
+
+// for (var i = 0; i <= 4; i++) {
+//     if (cityToCheck === cleanestCities[i]) {
+//         alert("It's one of the cleanest cities");
+//     }
+// }
+    
